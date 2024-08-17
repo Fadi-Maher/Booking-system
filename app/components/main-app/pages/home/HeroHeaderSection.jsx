ebbcf699a70bdaaa3fd75613c4f4fc04eb4f903a
hagar zaghloul <hagarzaghloul@gmail.com>
2024-08-18 02:19:21 +0300

    navbar edit & footer edit & home image edit

--- app/components/main-app/pages/home/HeroHeaderSection.jsx
+++ app/components/main-app/pages/home/HeroHeaderSection.jsx
@@ -1,15 +1,52 @@
-import styles from "./HeroHeaderSection.module.css";
+'use client';
+
+import React, { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
 
 const HeroHeaderSection = () => {
-  return (
-    <div className={styles["homepage-container"]}>
-      <img
-        className={styles.heroImg}
-        src="https://images.pexels.com/photos/189296/pexels-photo-189296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        alt="hero"
-      />
-    </div>
-  )
-}
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.bootstrap) {
+      window.bootstrap.Carousel.getInstance(document.querySelector('#carouselExampleSlidesOnly'));
+    }
+  }, []);
 
-export default HeroHeaderSection
+  return (
+    <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+      <div className="carousel-inner">
+        <div className="carousel-item active position-relative">
+          <img className="d-block w-100" src="/img/hero/hero-1.jpg" alt="First slide" />
+          <div className="carousel-caption d-none d-md-block">
+            <h1>Reserve Mate </h1>
+            <p>
+              At Reserve-Mate, we are dedicated to simplifying the reservation process for both 
+              businesses and customers. Our platform is designed to streamline bookings, making it easier and more efficient for everyone involved.
+            </p>
+          </div>
+        </div>
+        <div className="carousel-item position-relative">
+          <img className="d-block w-100" src="/img/hero/hero-2.jpg" alt="Second slide" />
+          <div className="carousel-caption d-none d-md-block">
+            <h1>Reserve Mate </h1>
+            <p>
+              At Reserve-Mate, we are dedicated to simplifying the reservation process for both
+               businesses and customers. Our platform is designed to streamline bookings, making it easier and more efficient for everyone involved.
+            </p>
+          </div>
+        </div>
+        <div className="carousel-item position-relative">
+          <img className="d-block w-100" src="/img/hero/hero-3.jpg" alt="Third slide" />
+          <div className="carousel-caption d-none d-md-block">
+            <h1>Reserve Mate </h1>
+            <p>
+              At Reserve-Mate, we are dedicated to simplifying the reservation process for both
+               businesses and customers. Our platform is designed to streamline bookings, making it easier and more efficient for everyone involved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroHeaderSection;
