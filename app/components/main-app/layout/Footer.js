e5e2dbabca2f3af8066c5c1681b27ad100c7bb68
Peter <Peter_Sadek@oulook.com>
2024-08-18 12:50:04 +0300

    Add logout functionality and display logged in username in navbar

--- app/components/main-app/layout/Footer.js
+++ app/components/main-app/layout/Footer.js
@@ -1,30 +1,37 @@
-import React from 'react';
-import styles from "./Footer.module.css"
+import React from "react";
+import styles from "./Footer.module.css";
 
 const Footer = () => {
-    return (
-			<footer className=" text-light py-5 footer " style={{ backgroundColor: "#222736" }}>
-				<div className="container">
-				<div className={styles.footertext}>
-					<div className="row">
-						<div className="col-lg-4 mb-4">
-						<div className={styles.ftAabout}>
-						<h4>our goal</h4>
-								<p> inspire and reach millions of travelers<br /> across 90 local websites</p>
-						</div>
-						</div>
-						<div className="col-lg-3 offset-lg-1 mb-4">
-						<div className={styles.ftnewslatter}>
-								<h4>New latest</h4>
-								<p>Get the latest updates and offers.</p>
-						</div>
-						<div class="text-center p-3" >© 2024 Reserve-Mate</div>
-						</div>
-						</div>
-					</div>
-					</div>
-			</footer>
-    );
-}
+  return (
+    <footer
+      className=" text-light py-5 footer "
+      style={{ backgroundColor: "#222736" }}
+    >
+      <div className="container">
+        <div className={styles.footertext}>
+          <div className="row">
+            <div className="col-lg-4 mb-4">
+              <div className={styles.ftAabout}>
+                <h4>our goal</h4>
+                <p>
+                  {" "}
+                  inspire and reach millions of travelers
+                  <br /> across 90 local websites
+                </p>
+              </div>
+            </div>
+            <div className="col-lg-3 offset-lg-1 mb-4">
+              <div className={styles.ftnewslatter}>
+                <h4>New latest</h4>
+                <p>Get the latest updates and offers.</p>
+              </div>
+              <div className="text-center p-3">© 2024 Reserve-Mate</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- app/logout/Logout.jsx
+++ app/logout/Logout.jsx
@@ -1,25 +1,28 @@
 "use client";
-import React from 'react'
+import React from "react";
 import { auth } from "@/app/firebase";
 import { useRouter } from "next/navigation";
 import { signOut } from "firebase/auth";
+import styles from "../components/main-app/ui/drawer/page.module.css";
 
-
-const Logout = () => { 
-    const router = useRouter()  
-    async function signOutUser() {
-      try {
-        await signOut(auth);
-        router.push("/login");
-      } catch (error) {
-        console.log(error);
-      }
+const Logout = () => {
+  const router = useRouter();
+  async function signOutUser() {
+    try {
+      await signOut(auth);
+      router.push("/login");
+    } catch (error) {
+      console.log(error);
     }
+  }
   return (
-    <div >
-    <button onClick={() => signOutUser()} className="btn btn-outline-warning text-white" type="submit">Logout</button>
+    <div>
+      {/* <button onClick={() => signOutUser()} className="btn btn-outline-warning text-white" type="submit">Logout</button> */}
+      <span onClick={() => signOutUser()} type="submit" className={styles.link}>
+        Logout
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default Logout
+export default Logout;
