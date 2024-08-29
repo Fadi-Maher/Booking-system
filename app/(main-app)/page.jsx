3258ee38aeb2a4371c653840ce92219c101ad22b
Oliver <olivermichel2011@gmail.com>
2024-08-29 12:07:05 +0300

    activate Reservation form

--- app/(main-app)/page.jsx
+++ app/(main-app)/page.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import AboutUsSection from '../components/main-app/pages/about-us/AboutUsSection';
-import HeroHeaderSection from '../components/main-app/pages/home/HeroHeaderSection';
-import SliderSection from '../components/main-app/pages/home/SliderSection';
-import ReservationForm from '../components/main-app/pages/calendar/Calendar';
+import React from "react";
+import AboutUsSection from "../components/main-app/pages/about-us/AboutUsSection";
+import HeroHeaderSection from "../components/main-app/pages/home/HeroHeaderSection";
+import SliderSection from "../components/main-app/pages/home/SliderSection";
+import ReservationForm from "../components/main-app/pages/calendar/Calendar";
 
 const HomePage = () => {
   return (
     <div>
       <HeroHeaderSection />
-      {/* <ReservationForm /> */}
+      <ReservationForm />
       <AboutUsSection />
       <SliderSection />
     </div>
