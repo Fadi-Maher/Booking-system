5032293887f875226c0722a4286e4bbf9d356adb
hagar zaghloul <hagarzaghloul@gmail.com>
2024-09-01 22:21:16 +0300

    handle payment

--- app/api/create-checkout-session/route.jsx
+++ app/api/create-checkout-session/route.jsx
@@ -1,30 +1,24 @@
 import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY); // Replace with your Stripe secret key
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+console.log(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(request) {
+  const { amount } = await request.json();
+
   try {
-    const { bookingId } = await request.json();
-
-    // You should retrieve booking details from your database based on the bookingId
-    const bookingDetails = { 
-      price: 1000, // Replace with the actual price from your booking details in cents
-      currency: 'usd', // Replace with the actual currency
-    };
-
-    // Create a PaymentIntent
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: bookingDetails.price,
-      currency: bookingDetails.currency,
+      amount: amount,
+      currency: 'usd',
       payment_method_types: ['card'],
-      metadata: { bookingId }, // Optional: Add metadata like booking ID
     });
 
-    // Return the client_secret to the frontend
-    return NextResponse.json({ clientSecret: paymentIntent.client_secret });
+    return NextResponse.json({
+      clientSecret: paymentIntent.client_secret,
+      sessionId: paymentIntent.id,
+    });
   } catch (error) {
-    console.error('Error creating PaymentIntent:', error);
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
