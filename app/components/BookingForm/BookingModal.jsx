3ff9705483a431e8369a008b5926c1c5480b84ab
Oliver <olivermichel2011@gmail.com>
2024-09-02 14:25:03 +0300

    updated booking modal functionality

--- app/components/BookingForm/BookingModal.jsx
+++ app/components/BookingForm/BookingModal.jsx
@@ -2,6 +2,9 @@
 import React, { useState, useEffect } from "react";
 import styles from "./BookingModal.module.css";
 import { useSearchParams } from "next/navigation";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import moment from "moment";
 
 const BookingModal = ({
   bookingDetails,
@@ -10,29 +13,47 @@ const BookingModal = ({
   onCancel,
 }) => {
   const searchParams = useSearchParams();
+
   const [arrivalDate, setArrivalDate] = useState(
-    searchParams.get("arrivalDate") || ""
+    searchParams.get("arrivalDate")
+      ? new Date(moment(searchParams.get("arrivalDate"), "DD-MM-YYYY").toDate())
+      : null
   );
   const [departureDate, setDepartureDate] = useState(
-    searchParams.get("departureDate") || ""
+    searchParams.get("departureDate")
+      ? new Date(
+          moment(searchParams.get("departureDate"), "DD-MM-YYYY").toDate()
+        )
+      : null
   );
   const [numberOfPersons, setNumberOfPersons] = useState(
     parseInt(searchParams.get("numberOfPersons")) || 1
+  );
+  const [numberOfChildren, setNumberOfChildren] = useState(
+    parseInt(searchParams.get("numberOfChildren")) || 0
   );
   const [numberOfNights, setNumberOfNights] = useState(
     parseInt(searchParams.get("numberOfNights")) || 1
   );
 
-  // Function to calculate the difference in days
+  // Update the total number of persons dynamically
+  useEffect(() => {
+    const totalPersons = numberOfPersons + numberOfChildren;
+    setBookingDetails(prevDetails => ({
+      ...prevDetails,
+      numberOfPersons: totalPersons,
+    }));
+  }, [numberOfPersons, numberOfChildren]);
+
   const calculateNights = (arrival, departure) => {
-    const arrivalDateObj = new Date(arrival);
-    const departureDateObj = new Date(departure);
-    const timeDiff = departureDateObj - arrivalDateObj;
-    const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
-    return daysDiff > 0 ? daysDiff : 1;
+    if (arrival && departure) {
+      const timeDiff = departure.getTime() - arrival.getTime();
+      const daysDiff = timeDiff / (1000 * 3600 * 24);
+      return daysDiff > 0 ? daysDiff : 1;
+    }
+    return 1;
   };
 
-  // Update number of nights whenever dates change
   useEffect(() => {
     if (arrivalDate && departureDate) {
       const calculatedNights = calculateNights(arrivalDate, departureDate);
@@ -44,40 +65,36 @@ const BookingModal = ({
     }
   }, [arrivalDate, departureDate]);
 
-  const handleArrivalDateChange = e => {
-    const newArrivalDate = e.target.value;
-    setArrivalDate(newArrivalDate);
+  const handleArrivalDateChange = date => {
+    setArrivalDate(date);
     setBookingDetails(prevDetails => ({
       ...prevDetails,
-      arrivalDate: newArrivalDate,
+      arrivalDate: moment(date).format("DD-MM-YYYY"),
     }));
+    if (departureDate && date && departureDate <= date) {
+      setDepartureDate(null);
+    } else {
+      setNumberOfNights(calculateNights(date, departureDate));
+    }
   };
 
-  const handleDepartureDateChange = e => {
-    const newDepartureDate = e.target.value;
-    setDepartureDate(newDepartureDate);
+  const handleDepartureDateChange = date => {
+    setDepartureDate(date);
     setBookingDetails(prevDetails => ({
       ...prevDetails,
-      departureDate: newDepartureDate,
+      departureDate: moment(date).format("DD-MM-YYYY"),
     }));
+    setNumberOfNights(calculateNights(arrivalDate, date));
   };
 
-  const handleNumberOfPersonsChange = e => {
-    const newNumberOfPersons = parseInt(e.target.value);
-    setNumberOfPersons(newNumberOfPersons);
-    setBookingDetails(prevDetails => ({
-      ...prevDetails,
-      numberOfPersons: newNumberOfPersons,
-    }));
+  const handleNumberOfPersonsChange = event => {
+    const value = Math.max(1, Number(event.target.value));
+    setNumberOfPersons(value);
   };
 
-  const handleNumberOfNightsChange = e => {
-    const newNumberOfNights = parseInt(e.target.value);
-    setNumberOfNights(newNumberOfNights);
-    setBookingDetails(prevDetails => ({
-      ...prevDetails,
-      numberOfNights: newNumberOfNights,
-    }));
+  const handleNumberOfChildrenChange = event => {
+    const value = Math.max(0, Number(event.target.value));
+    setNumberOfChildren(value);
   };
 
   return (
@@ -86,65 +103,83 @@ const BookingModal = ({
         <div>
           <h2 className="mb-4 text-center">Book Room</h2>
 
-          {arrivalDate && (
-            <div className={styles.formGroup}>
-              <label>Arrival Date: {arrivalDate}</label>
-            </div>
-          )}
+          {/* Labels Section */}
+          <div className={styles.labelsSection}>
+            {arrivalDate && (
+              <p className={styles.label}>
+                Arrival Date: {moment(arrivalDate).format("DD-MM-YYYY")}
+              </p>
+            )}
+            {departureDate && (
+              <p className={styles.label}>
+                Departure Date: {moment(departureDate).format("DD-MM-YYYY")}
+              </p>
+            )}
+            <p className={styles.label}>Number Of Nights: {numberOfNights}</p>
+            <p className={styles.label}>Number Of Adults: {numberOfPersons}</p>
+            {numberOfChildren > 0 && (
+              <p className={styles.label}>
+                Number Of Children: {numberOfChildren}
+              </p>
+            )}
+            <p className={styles.label}>
+              Total Number Of Persons: {numberOfPersons + numberOfChildren}
+            </p>
+          </div>
 
-          {departureDate && (
-            <div className={styles.formGroup}>
-              <label>Departure Date: {departureDate}</label>
-            </div>
-          )}
-
-          {numberOfNights && (
-            <div className={styles.formGroup}>
-              <label>Number Of Nights: {numberOfNights}</label>
-            </div>
-          )}
-
-          {numberOfPersons && (
-            <div className={styles.formGroup}>
-              <label>Number Of Persons: {numberOfPersons}</label>
-            </div>
-          )}
-
+          {/* Input Fields Section */}
           <div className={styles.formGroup}>
-            <label>Arrival Date: </label>
-            <input
-              type="date"
-              value={arrivalDate}
+            <label>Arrival Date:</label>
+            <DatePicker
+              selected={arrivalDate}
               onChange={handleArrivalDateChange}
+              className="form-control"
+              placeholderText="Select Arrival Date"
+              dateFormat="dd/MM/yyyy"
+              minDate={new Date()}
             />
           </div>
 
           <div className={styles.formGroup}>
-            <label>Departure Date: </label>
-            <input
-              type="date"
-              value={departureDate}
+            <label>Departure Date:</label>
+            <DatePicker
+              selected={departureDate}
               onChange={handleDepartureDateChange}
+              className="form-control"
+              placeholderText="Select Departure Date"
+              dateFormat="dd/MM/yyyy"
+              minDate={arrivalDate || new Date()}
+              disabled={!arrivalDate}
             />
           </div>
 
           <div className={styles.formGroup}>
-            <label>Number Of Nights: </label>
+            <label>Number Of Nights:</label>
             <input
               type="number"
-              min="1"
               value={numberOfNights}
-              onChange={handleNumberOfNightsChange}
+              readOnly
+              style={{ backgroundColor: "#e4ba90", color: "white" }}
             />
           </div>
 
           <div className={styles.formGroup}>
-            <label>Number Of Persons: </label>
+            <label>Number Of Adults:</label>
             <input
               type="number"
-              min="1"
               value={numberOfPersons}
               onChange={handleNumberOfPersonsChange}
+              min="1"
+            />
+          </div>
+
+          <div className={styles.formGroup}>
+            <label>Number Of Children:</label>
+            <input
+              type="number"
+              value={numberOfChildren}
+              onChange={handleNumberOfChildrenChange}
+              min="0"
             />
           </div>
 
