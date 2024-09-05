2b57815f0835fa0bf85afca3027e723b25fecd70
Oliver <olivermichel2011@gmail.com>
2024-09-05 19:48:55 +0300

    firebase configuration

--- app/firebase.jsx
+++ app/firebase.jsx
@@ -7,16 +7,25 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDqSz-4ReapV0YhX-qy0_arqrYW7rlK69g",
-  authDomain: "reservemate-74770.firebaseapp.com",
-  databaseURL: "https://reservemate-74770-default-rtdb.firebaseio.com",
-  projectId: "reservemate-74770",
-  storageBucket: "reservemate-74770.appspot.com",
-  messagingSenderId: "413530914869",
-  appId: "1:413530914869:web:81c9bee00e416b2ea0f17c",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDqSz-4ReapV0YhX-qy0_arqrYW7rlK69g",
+//   authDomain: "reservemate-74770.firebaseapp.com",
+//   databaseURL: "https://reservemate-74770-default-rtdb.firebaseio.com",
+//   projectId: "reservemate-74770",
+//   storageBucket: "reservemate-74770.appspot.com",
+//   messagingSenderId: "413530914869",
+//   appId: "1:413530914869:web:81c9bee00e416b2ea0f17c",
+// };
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDnDg2ObdV3BkirDdoZGmMfOuxDbXcv6YY",
+  authDomain: "reserve-mate-18e59.firebaseapp.com",
+  projectId: "reserve-mate-18e59",
+  storageBucket: "reserve-mate-18e59.appspot.com",
+  messagingSenderId: "37718736390",
+  appId: "1:37718736390:web:9cf1d93bd79fc79bf9cd87",
+  measurementId: "G-WWRC77KQMC",
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
